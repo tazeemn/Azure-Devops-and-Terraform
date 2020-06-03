e412963212778ddc831729a66f3ace0ad5f9092d
Tazeem Naqvi <tazeem@outlook.com>
2020-06-03 21:38:55 +1000

    updated Azure Pipeline

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ provider "azurerm" {
 
 }
 
+terraform {
+    backend "azurerm" {
+        resource_group_name     = "tf_rg_blobstore"
+        storage_account_name    = "tfstoragetazeem"
+        container_name          = "tfstate"
+        key                     = "terraform.tfstate"
+    }
+}
 resource "azurerm_resource_group" "main" {
     name = "tfmainrg"
     location = "Australia East"
